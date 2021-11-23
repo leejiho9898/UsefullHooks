102980381f8025ca3b0e8624e7d0836d9e618ba8
이지호 <cctv1998@naver.com>
2021-11-23 21:33:51 +0900

    Feat : useClick

--- client/src/hooks/useClick.ts
+++ client/src/hooks/useClick.ts
@@ -1,5 +1,6 @@
-import React, { MutableRefObject, useEffect } from "react";
+import React, { useEffect } from "react";
 
+/** element를 클릭할 시점에 파라미터로 들어온 함수를 실행함 */
 export default function useClick(cb: () => void) {
   const element = React.useRef<HTMLDivElement>(null);
   useEffect(() => {
@@ -18,3 +19,5 @@ export default function useClick(cb: () => void) {
 
   return element;
 }
+// 여기서 리턴한 element를 ref로 설정한다
+// 그런다음 해당 HTMLDivElement를 누르면 cb가 실행됨
