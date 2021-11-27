7135fee6502d0a3c292643ecc0b02276005e2540
이지호 <cctv1998@naver.com>
2021-11-28 02:52:57 +0900

    Feat : useFetch

---
+++ client/src/hooks/useFetch.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useAxios(url: string) {
+  const [value, setValue] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setValue(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
+
+  const refetch = () => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setValue(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return { value, loading, error, refetch };
+}
