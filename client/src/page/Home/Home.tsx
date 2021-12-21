180c25359fc31d23252aa1a512dcf70796d33cb1
이지호 <cctv1998@naver.com>
2021-12-22 03:51:35 +0900

    Test

--- client/src/page/Home/Home.tsx
+++ client/src/page/Home/Home.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import requestPayMents from "../../hooks/usePay";
 
 const Home = () => {
+  // * 테스트용 클라이언트 키로 시작하세요
+
+  const item = {
+    amount: 1,
+    orderId: "brL9yXo4bVrpAHe7p5sgZ",
+    orderName: "토스 티셔츠 외 2건",
+    customerName: "박토스",
+    successUrl: "http://localhost:3000/success",
+    failUrl: "http://localhost:8080/fail",
+  };
   return (
-    <div>
-      <Link to="/register">React-Hook-Form</Link>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          requestPayMents(item);
+        }}
+      >
+        야 ㅋㅋ 처 눌러봐 병신아
+      </button>
+    </>
   );
 };
 
