a41d593b5416822cee58bb0050e4a202c98f5636
이지호 <cctv1998@naver.com>
2022-04-29 23:38:46 +0900

    Feat : toggle

--- client/src/page/pay/Pay.tsx
+++ client/src/page/pay/Pay.tsx
@@ -1,8 +1,16 @@
+import { useForm, useToggle } from "@react-tool/hooks";
 import requestPayMents from "../../hooks/usePay";
 
 const Pay = () => {
   // * 테스트용 클라이언트 키로 시작하세요
-
+  const { form, onChangeForm, onResetForm } = useForm({
+    name: "",
+    email: "",
+    password: "",
+  });
+  console.log(form);
+  
+  const [istoggle, onToggle] = useToggle(false);
   const item = {
     amount: 1,
     orderId: "brL9yXo4bVrpAHe7p5sgZ",
@@ -20,6 +28,13 @@ const Pay = () => {
       >
         눌러보세요
       </button>
+      <button onClick={onToggle}>토글</button>
+      {istoggle ? <p>Hello</p> : <p>Bye</p>}
+
+      <input value={form.name} onChange={onChangeForm} name="name" />
+      <input value={form.email} onChange={onChangeForm} name="email" />
+      <input value={form.password} onChange={onChangeForm} name="password" />
+      <button onClick={onResetForm}>Reset</button>
     </>
   );
 };
